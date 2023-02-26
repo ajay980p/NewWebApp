e3c75f1effa4b2564c5d8f61c8bd65e8e7645b48
unknown <ajay980p@gmail.com>
2023-02-26 11:09:13 +0530

    Horizontal Scroll added-Working fine

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header/Header'
 import Categories from './Categories/Categories'
 import Top_News_Grid from './Top_News_Grid/Top_News_Grid'
 import NewsOfTheDay from './NewsOfTheDay/NewsOfTheDay';
+import AutoScrollable from './AutoScroll/AutoScrollable';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Categories />
       <Top_News_Grid />
       <NewsOfTheDay />
+      <AutoScrollable />
     </div>
   );
 }
---
+++ src/AutoScroll/AutoScrollable.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './AutoScrollable.css'
+
+function AutoScrollable() {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <div className='wrapper'>
+        <div className='items'>box-1</div>
+        <div className='items'>box-2</div>
+        <div className='items'>box-3</div>
+        <div className='items'>box-4</div>
+        <div className='items'>box-5</div>
+        <div className='items'>box-6</div>
+        <div className='items'>box-7</div>
+        <div className='items'>box-8</div>
+        <div className='items'>box-9</div>
+      </div>
+    </div>
+  )
+}
+
+export default AutoScrollable;
